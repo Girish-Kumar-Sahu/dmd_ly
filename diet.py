f7946acf4f931cec7ca439e46b20ffb2497ed589
Girish-Kumar-Sahu <girishkumarsahu2008@gmail.com>
2026-02-27 21:02:56 +0530

    lyrics added

---
+++ diet.py
@@ -0,0 +1,16 @@
+import time
+
+# Lyrics from 45-60 seconds of "Diet Mountain Dew" by Lana Del Rey (Verse 1 segment)
+lyrics = [
+    "Baby, put on heart-shaped sunglasses",
+    "Cause we gonna take a ride",
+    "I'm not gonna listen to what the past says",
+    "I been waitin' up all night",
+    "Take another drag, turn me to ashes",
+    "Ready for another lie?"
+]
+
+
+for line in lyrics:
+    print(line)
+    time.sleep(2.5)  # Approx 2.5s per line to 
